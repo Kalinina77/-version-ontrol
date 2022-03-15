7fa32be91966361f513c1753ad98bada59a647a5
Kalinina77 <isip_a.v.kalinina@mpt.ru>
2022-03-15 13:25:53 +0300

    first commit

--- app/src/main/java/com/example/wsr/MainActivity.kt
+++ app/src/main/java/com/example/wsr/MainActivity.kt
@@ -62,7 +62,8 @@ class MainActivity : AppCompatActivity() {
             .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
-            .build()
+
+
 
         return GoogleSignIn.getClient(this, gso)
     }
